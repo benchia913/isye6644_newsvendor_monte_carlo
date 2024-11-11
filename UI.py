41d57eb602091cffbe4bf841f15ae8d86aa15f31
Chia Kok Ben <chiakokben@gmail.com>
2024-11-12 07:16:41 +0800

    UI

--- UI.py
+++ UI.py
@@ -1,14 +1,50 @@
 import ipywidgets as ipw
 from UI_constants import *
 
+###############
+# ASSUMPTIONS #
+###############
+
+
+
+
+
+
+###########
+# MAIN UI #
+###########
+
+
 
 # input widgets
+
+# input box 1 - Random Variable Generation
+
+w_header_rv_gen = ipw.HTML(
+    value='<b><u>Random Variable Generation</u></b>'
+)
+
 w_random_seed = ipw.IntText(
     value=0, 
     description='Random Seed: ', 
     disabled=False, 
     style=INT_TEXT_STYLE, 
     layout=INT_TEXT_LAYOUT
+)
+
+
+w_num_iterations = ipw.IntText(
+    value=100_000, 
+    description='Number of Iterations: ', 
+    disabled=False, 
+    style=INT_TEXT_STYLE, 
+    layout=INT_TEXT_LAYOUT
+)
+
+# input box 2 - Demand Distribution
+
+w_header_demand_dist = ipw.HTML(
+    value='<b><u>Demand Distribution</b></u>'
 )
 
 w_demand_mean = ipw.IntText(
@@ -27,12 +63,10 @@ w_demand_std_dev = ipw.IntText(
     layout=INT_TEXT_LAYOUT
 )
 
-w_num_iterations = ipw.IntText(
-    value=0, 
-    description='Number of Iterations: ', 
-    disabled=False, 
-    style=INT_TEXT_STYLE, 
-    layout=INT_TEXT_LAYOUT
+# input box 3 - Unit Economics
+
+w_header_unit_economics = ipw.HTML(
+    value='<b><u>Unit Economics</b></u>'
 )
 
 w_unit_cost = ipw.IntText(
@@ -59,9 +93,15 @@ w_unit_salvage_value = ipw.IntText(
     layout=INT_TEXT_LAYOUT
 )
 
+# input box 4 - Decision Parameters
+
+w_header_decision_params = ipw.HTML(
+    value='<b><u>Decision Parameters</b></u>'
+)
+
 w_order_quantity = ipw.IntText(
     value=0, 
-    description='Demand Mean: ', 
+    description='Order Quantity: ', 
     disabled=False, 
     style=INT_TEXT_STYLE, 
     layout=INT_TEXT_LAYOUT
@@ -70,9 +110,21 @@ w_order_quantity = ipw.IntText(
 
 # assemble main widget object
 w_main_UI = ipw.VBox([
-    w_random_seed, w_num_iterations, 
-    w_demand_mean, w_demand_std_dev, 
-    w_unit_cost, w_unit_sale_price, w_unit_salvage_value, 
-    w_order_quantity, 
-    ])
+                ipw.HBox([
+                    ipw.VBox([w_header_rv_gen, 
+                              w_random_seed, w_num_iterations, ], 
+                             layout=CONTAINER_LAYOUT), 
+                    ipw.VBox([w_header_demand_dist, 
+                              w_demand_mean, w_demand_std_dev, ], 
+                             layout=CONTAINER_LAYOUT), 
+                ]), 
+                ipw.HBox([
+                    ipw.VBox([w_header_unit_economics, 
+                              w_unit_cost, w_unit_sale_price, w_unit_salvage_value, ], 
+                             layout=CONTAINER_LAYOUT),  
+                    ipw.VBox([w_header_decision_params, 
+                              w_order_quantity, ], 
+                             layout=CONTAINER_LAYOUT)
+                ])
+            ])
 
--- UI_constants.py
+++ UI_constants.py
@@ -1,4 +1,11 @@
+from ipywidgets import Layout
+
 INT_TEXT_STYLE = {'description_width': '200px'
                   }
 
 INT_TEXT_LAYOUT = {'width': '400px'}
+
+CONTAINER_LAYOUT = Layout(border='solid 2px',
+                          margin='3px 3px 3px 3px',
+                          padding='10px 10px 10px 10px'
+                          )
